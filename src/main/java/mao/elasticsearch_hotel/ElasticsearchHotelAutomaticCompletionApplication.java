267b006df9c4087c6e1177e8f7dff479606c1ac9
mao <1296193245@qq.com>
2022-06-03 12:59:39 +0800

    elasticsearch_hotel_automatic_completion

---
+++ src/main/java/mao/elasticsearch_hotel/ElasticsearchHotelAutomaticCompletionApplication.java
@@ -0,0 +1,15 @@
+package mao.elasticsearch_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchHotelAutomaticCompletionApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ElasticsearchHotelAutomaticCompletionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/elasticsearch_hotel/ElasticsearchHotelAutomaticCompletionApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.elasticsearch_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchHotelAutomaticCompletionApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
